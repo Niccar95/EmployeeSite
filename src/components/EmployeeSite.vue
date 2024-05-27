0fabe0eb2b81f99232612c3fe0ce71d5af3b34af
Niccar95 <niccar.95@outlook.com>
2024-05-27 21:28:03 +0200

    added state for EmployeeState and tested api response

--- src/components/EmployeeSite.vue
+++ src/components/EmployeeSite.vue
@@ -1,3 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { IEmployeeState } from "@/models/IEmployeeState";
+import { allEmployeeData } from "@/services/EmployeeService";
+import { onMounted, ref } from "vue";
+
+const employeeState = ref<IEmployeeState>({
+  page: 1,
+  per_page: 6,
+  total: 12,
+  total_pages: 2,
+  data: [],
+});
+
+onMounted(async () => {
+  employeeState.value = await allEmployeeData();
+  console.log(employeeState.value.data);
+});
+</script>
+
 <template></template>
+
 <style scoped></style>
